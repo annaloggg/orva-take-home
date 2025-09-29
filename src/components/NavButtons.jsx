0a9dbe17202c55a7756200313f2ab0cfa47dc117
annaloggg <atp49@pitt.edu>
2025-09-29 17:33:25 -0400

    updated readme and some styling

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -31,18 +31,16 @@ export const NavButtons = ({ numRepos, repos, setRepos, currentPage, setCurrentP
         const link = repos.headers.link;
         const buttonStyle = "mx-2 py-2 px-4 bg-stone-200 rounded-full"
 
+        // extract links to previous, next, and last pages from link header
         const prevPattern = link.match(/(?<=<)([\S]*)(?=>; rel="prev")/i);
         const nextPattern = link.match(/(?<=<)([\S]*)(?=>; rel="next")/i);
         const lastPattern = link.match(/(?<=<)([\S]*)(?=>; rel="last")/i);
         const lastPage = getLastPage(lastPattern);
 
         return (
-            <div className="py-6 flex flex-row justify-center items-center">
-
+            <div className="inset-shadow-sm py-6 flex flex-row justify-center items-center">
                 <button disabled={currentPage == 1} className={buttonStyle + (currentPage === 1 ? " cursor-not-allowed opacity-50" : " hover:bg-stone-300 ")} onClick={handlePrevRepos}>prev</button>
-
                 <h6>{currentPage}</h6>
-
                 <button disabled={currentPage >= lastPage} className={buttonStyle + (currentPage >= lastPage ? " cursor-not-allowed opacity-50" : " hover:bg-stone-300 ")} onClick={handleNextRepos}>next</button>
             </div>
         )
